88a2806f45a17c357427d33f49023391374a6c2b
Son Nguyen <nguyenson120296@gmail.com>
2017-12-09 10:35:47 +0700

    update ui

--- src/traynotification/AnimationProvider.java
+++ src/traynotification/AnimationProvider.java
@@ -6,13 +6,6 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-/**
- * A provider class that houses all animation objects that implement TrayAnimation
- * This makes it easier to lookup any existing animations and retrieve them
- * This means the user can easily switch animations, and in the background all possible animation types
- * Are stores in here, so when a user selects an animation type, this class will return the TrayAnimation that satisfies that request
- * It's much like a mini database
- */
 public class AnimationProvider {
 
     private List<TrayAnimation> animationsList;
